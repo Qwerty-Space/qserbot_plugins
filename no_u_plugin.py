651e8187af90e46bb70f8c7578a0ae93ae50ab60
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-09 20:34:21 +0100

    Create no_u_plugin.py

---
+++ no_u_plugin.py
@@ -0,0 +1,8 @@
+from telethon import events, sync
+from global_functions import probability
+
+async def no_u(event):
+    if event.is_private:
+        await event.reply(f"no {event.pattern_match.string}".lower())
+
+no_u.event = events.NewMessage(pattern=re.compile(r"(no ){1,50}(u|you)$", re.I).match)
