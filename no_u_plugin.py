2e9d96b9e696e01476146720a88df88252d8b2f9
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-10 08:51:16 +0100

    Don't handle outgoing messages

--- no_u_plugin.py
+++ no_u_plugin.py
@@ -6,4 +6,4 @@ async def no_u(event):
     if event.is_private:
         await event.reply(f"no {event.pattern_match.string}".lower())
 
-no_u.event = events.NewMessage(pattern=re.compile(r"(no ){1,50}(u|you)$", re.I).match)
+no_u.event = events.NewMessage(pattern=re.compile(r"(no ){1,50}(u|you)$", re.I).match, outgoing=False)
