44b816d093118f935d49637e8c2385a9de339efc
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-19 17:09:44 +0100

    Doesn't need to be in private tbh

--- ping_plugin.py
+++ ping_plugin.py
@@ -5,11 +5,10 @@ from global_functions import probability
 # /ping
 async def ping_pong(event):
     sender = await event.get_sender()
-    if event.is_private:
-        a = datetime.timestamp(datetime.now())
-        await event.edit("**Pong!**")
-        b = datetime.timestamp(datetime.now()) - a
-        print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string} [{b:.3f}]")
-        await event.edit(f"**Pong!**\nTook `{b:.3f}` seconds")
+    a = datetime.timestamp(datetime.now())
+    await event.edit("**Pong!**")
+    b = datetime.timestamp(datetime.now()) - a
+    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string} [{b:.3f}]")
+    await event.edit(f"**Pong!**\nTook `{b:.3f}` seconds")
 
 ping_pong.event = events.NewMessage(pattern=r"(/uping)$", incoming=False)
