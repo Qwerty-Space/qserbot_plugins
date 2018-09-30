18276d7742394f18f64f757fdf7e06a07144ca59
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-09-30 08:14:15 +0100

    Update foobar_plugin.py

--- foobar_plugin.py
+++ foobar_plugin.py
@@ -7,6 +7,6 @@ async def foobar(event):
     print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
     if event.from_id == 232787997:
         return
-    await event.reply("^bar$")
+    await event.reply("bar")
 
-foobar.event = events.NewMessage(pattern="foo", chats=1001040270887, outgoing=False)
+foobar.event = events.NewMessage(pattern="^foo$", chats=1001040270887, outgoing=False)
