439cfb129661907e446f469de95e234662daa2d9
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-04 20:43:36 +0100

    Supports more variations

--- no_u_plugin.py
+++ no_u_plugin.py
@@ -4,6 +4,6 @@ from global_functions import probability
 
 async def no_u(event):
     if event.is_private:
-        await event.reply(f"no {event.pattern_match.string}".lower())
+        await event.reply(f"{event.pattern_match.group(1)}{event.pattern_match.string}".lower())
 
-no_u.event = events.NewMessage(pattern=re.compile(r"(no ){1,50}(u|you)$", re.I).match, outgoing=False)
+no_u.event = events.NewMessage(pattern=re.compile(r"(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew))$", re.I).match, outgoing=False)
