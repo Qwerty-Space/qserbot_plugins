81362bbb1a7c39daf92ca83a61d2cc7887729317
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-04 20:52:28 +0100

    Fixed regex

--- no_u_plugin.py
+++ no_u_plugin.py
@@ -6,4 +6,4 @@ async def no_u(event):
     if event.is_private:
         await event.reply(f"{event.pattern_match.group(1)}{event.pattern_match.string}".lower())
 
-no_u.event = events.NewMessage(pattern=re.compile(r"(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew))$", re.I).match, outgoing=False)
+no_u.event = events.NewMessage(pattern=re.compile(r"(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$", re.I).match, outgoing=False)
