0cdce642b29f4115fef94e4aa5705266258291ba
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-11 19:14:48 +0100

    tping → uping

--- uping_plugin.py
+++ uping_plugin.py
@@ -1,14 +1,16 @@
 """The bot will edit a message with the time it took to respond to your command.
+Only responds to the person running the script.
 
-pattern:  /uping$
+pattern:  `/uping$`
 """
 
 from datetime import datetime
 from telethon import events, sync
 from .global_functions import probability
 
+
 # /ping
-@events.register(events.NewMessage(pattern=r"/tping$", incoming=False))
+@events.register(events.NewMessage(pattern=r"/uping$", incoming=False))
 async def ping_pong(event):
     sender = await event.get_sender()
     a = datetime.timestamp(datetime.now())
