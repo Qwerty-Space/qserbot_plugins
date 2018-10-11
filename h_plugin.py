749f593a1e7d13c61324cd4e9f2b6008f08bf652
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-11 19:18:54 +0100

    lazy

--- h_plugin.py
+++ h_plugin.py
@@ -1,14 +1,15 @@
-"""Responds to "h" with a sticker.  Only from certain users.
+"""Responds to "h" with a sticker.  Only in private.
 
-pattern: `h+$`
+pattern:  `h+$`
 """
 
 from .global_functions import probability
 from telethon import events, sync
 
 
-@events.register(events.NewMessage(pattern="h+$", chats=[232787997,51863899], outgoing=False))
+@events.register(events.NewMessage(pattern="h+$", outgoing=False))
 async def on_h(event):
-    sender = await event.get_sender()
-    print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
-    await event.reply(file="CAADAgADxSgAAuCjggcKB413JNAx6wI")
+    if event.is_private:
+        sender = await event.get_sender()
+        print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
+        await event.reply(file="CAADAgADxSgAAuCjggcKB413JNAx6wI")
