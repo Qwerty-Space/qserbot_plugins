d15e4501189805906df7483443f96a25c6a46f87
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-12 23:30:31 +0100

    No forwards

--- no_u_plugin.py
+++ no_u_plugin.py
@@ -6,7 +6,7 @@ pattern:  `(?i)(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$`
 from telethon import events, sync
 
 
-@events.register(events.NewMessage(pattern=r"(?i)(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$", outgoing=False))
+@events.register(events.NewMessage(pattern=r"(?i)(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$", outgoing=False, fowards=False))
 async def no_u(event):
     me = await event.client.get_me()
     if event.is_reply:
@@ -17,7 +17,7 @@ async def no_u(event):
             replied_to = (await event.client.get_messages(event.chat_id, ids=prev_id)).from_id
         except AttributeError:
             replied_to = None
-    if replied_to == me.id:
+    if event.is_private or replied_to == me.id:
         await event.reply(f"{event.pattern_match.group(1)}{event.pattern_match.string}".lower())
         sender = await event.get_sender()
         print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
