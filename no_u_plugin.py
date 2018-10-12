6d47a52a28e0381da630168dd3bb65557fb9a589
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-13 00:40:24 +0100

    I regret it

--- no_u_plugin.py
+++ no_u_plugin.py
@@ -8,16 +8,5 @@ from telethon import events, sync
 
 @events.register(events.NewMessage(pattern=r"(?i)(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$", outgoing=False, fowards=False))
 async def no_u(event):
-    me = await event.client.get_me()
-    if event.is_reply:
-        replied_to = (await event.get_reply_message()).from_id
-    else:
-        try: 
-            prev_id = (event.id) - 1
-            replied_to = (await event.client.get_messages(event.chat_id, ids=prev_id)).from_id
-        except AttributeError:
-            replied_to = None
-    if event.is_private or replied_to == me.id:
+    if event.is_private:
         await event.reply(f"{event.pattern_match.group(1)}{event.pattern_match.string}".lower())
-        sender = await event.get_sender()
-        print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
