4ef88510f010a8c8ca8309c0000631139541b92c
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2018-10-14 21:17:17 +0100

    Update foobar_plugin.py

--- foobar_plugin.py
+++ foobar_plugin.py
@@ -5,10 +5,10 @@ from .global_functions import probability
 from telethon import events, sync
 
 
-@events.register(events.NewMessage(pattern="foo", chats=1001040270887, outgoing=False))
+@events.register(events.NewMessage(pattern="^foo$", chats=1001040270887, outgoing=False))
 async def foobar(event):
     sender = await event.get_sender()
     print(f"[{event.date.strftime('%c')}] [{sender.id}] {sender.username}: {event.pattern_match.string}")
     if event.from_id == 232787997:
         return
-    await event.reply("^bar$")
+    await event.reply("bar")
