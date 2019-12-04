ae4b073b4238c9c5a1fa04dd9fc715ab1578b05b
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2019-12-04 23:10:23 +0000

    Deletes private messages about my new pfp

---
+++ pfp_plugin.py
@@ -0,0 +1,33 @@
+"""Start message
+
+Makes people stfu about my new profile pic in PM
+"""
+
+from telethon import client, events
+from .global_functions import log
+from fuzzywuzzy import fuzz
+import re
+
+
+# Fuzzy checcks the message
+def fuzzyfinder(msg, pfp):
+    if fuzz.partial_ratio(f"you changed your {pfp}", msg) >= 80:
+        return True
+
+    if fuzz.partial_ratio(f"you got a new {pfp}", msg) >= 80:
+        return True
+
+    if fuzz.partial_ratio(f"you have a new {pfp}", msg) >= 80:
+        return True
+
+
+# hi
+@events.register(events.NewMessage(pattern=re.compile(r"\b((pf|p(rofile)?|d(isplay)?) ?(p(ic(ture)?|ima?ge?)?)?\b)").search, incoming=True))
+async def on_pfp(event):
+    if event.is_private:    # If command was sent in private
+        msg = event.pattern_match.string
+        pfp = event.pattern_match.group(1)
+        fuzzyfinder(msg, pfp)
+
+        await log(event, msg)    # Logs the event
+        await event.delete()
