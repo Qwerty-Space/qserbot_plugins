232a451a30a3d808c697183eef6c366353537828
Qwerty-Space <Qwerty-Space@users.noreply.github.com>
2020-07-05 02:44:36 +0100

    Update no_u_plugin.py

--- no_u_plugin.py
+++ no_u_plugin.py
@@ -6,9 +6,13 @@ pattern:  `(?i)(n\w+h? ){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$`
 from .global_functions import log
 from telethon import events, sync
 
-
-@events.register(events.NewMessage(pattern=r"(?i)(n\w+h?[\s\u2063]+){1,50}(\w?(?:[uü]+|y(?:ou|ew)))$", outgoing=False, forwards=False))
+@events.register(events.NewMessage(
+        pattern=r"(?i)(n(o|ein|ada)+[\s\u2063]+){1,50}([bdj]?(?:[uü]+|y\s?(?:o\s?u|e\s?w)))", 
+        outgoing=False, forwards=False)
+        )
 async def no_u(event):
-    if event.is_private:
-        await event.reply(f"{event.pattern_match.group(1)}{event.pattern_match.string}".lower())
-        await log(event)
+    if not event.is_private:
+        return
+
+    await event.reply(f"{event.pattern_match.group(1)}{event.pattern_match.string}".lower())
+    await log(event)
